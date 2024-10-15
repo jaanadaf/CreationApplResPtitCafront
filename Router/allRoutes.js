f85af7e5ec47dfb0598c6c4c7c6790ee15052fa4
jaadar <abdelkaderjaadar@yahoo.fr>
2024-10-15 12:50:27 +0200

    réalisation de page de connection

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html")
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "connection", "/pages/signin.html")
 
 ];
 
